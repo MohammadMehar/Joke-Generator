cfed3bad80b8d36afbd48fc6ae9446df5097f930
MohammadMehar <kirtomits@gmail.com>
2024-06-25 19:52:03 -0400

    Update joke.js

--- joke.js
+++ joke.js
@@ -1,24 +1,33 @@
 var jokePar = document.getElementById("joke")
-
+const checkBox = document.getElementById("checker")
 
 
 document.getElementById("generateButton").onclick = async function() {
-    fetch("https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=twopart")
-    .then(res => {
-        return res.json()
-    })
-    .then(data => {
-        console.log(data["setup"]);
-        console.log(data["delivery"]);
+    if (checkBox.checked) {
+        console.log(checkBox.checked)
+        fetch("https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=twopart")
+        .then(res => {
+            return res.json()
+        })
+        .then(data => {
+            console.log(data["setup"]);
+            console.log(data["delivery"]);
+    
+            jokePar.innerText = data["setup"] + " \n " + data["delivery"]
+        })
+    }else {
+        fetch("https://v2.jokeapi.dev/joke/Any?type=twopart")
+        .then(res => {
+            return res.json()
+        })
+        .then(data => {
+            console.log(data["setup"]);
+            console.log(data["delivery"]);
+    
+            jokePar.innerText = data["setup"] + " \n " + data["delivery"]
+        })
+    }
+};
 
-        var jokeText = jokePar.innerText = data["setup"] + " \n " + data["delivery"]
-        var current = ""
-        for (i=1; i != jokeText.length; i++) {
-            current = current + jokeText[i-1]
-            jokePar.innerText = current
 
-        }
-
-        /*jokePar.innerText = data["setup"] + " \n " + data["delivery"]*/
-    })
 };
